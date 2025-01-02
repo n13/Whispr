779f4d4b503084075d1c24b012dc33698c24313f
NIK <nheger@gmail.com>
2025-01-03 07:34:22 +0800

    Initial Commit

---
+++ Whispr/WhisprApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisprApp.swift
+//  Whispr
+//
+//  Created by Nikolaus Heger on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct WhisprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WhisprTests/WhisprTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisprTests.swift
+//  WhisprTests
+//
+//  Created by Nikolaus Heger on 1/3/25.
+//
+
+import Testing
+@testable import Whispr
+
+struct WhisprTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
